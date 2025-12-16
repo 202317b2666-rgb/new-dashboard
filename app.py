a6ce91ffa44deebc0f14ab8a0ac0adb594e7fb56
Guru <202317b2666@wilp.bits-pilani.ac.in>
2025-12-16 15:20:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -458,9 +458,9 @@ try:
             style_function=style_function, 
             # Hover highlight set to a dark color to contrast with the light CSS border
             highlight_function=lambda x: {
-                'weight': 2,          
+                'weight': 3,          
                 'color': "#00000062",    
-                'fillOpacity': 0.4
+                'fillOpacity': 0.5
             }, 
             tooltip=folium.features.GeoJsonTooltip(fields=['name'], aliases=['Country Name:']),
         ).add_to(m)
